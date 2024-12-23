0a2fc6728091dd10698305e42e029d646ec903ea
khushi <khushiiii4321@gmail.com>
2024-12-23 17:13:40 +0530

    Initial commit

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from './Hero'
+import PopularProduct from './PopularProduct'
+import Footer from './Footer'
+import Services from './Services'
+import SpecialOffer from './SpecialOffer'
+import Subscribe from './Subscribe'
+import SuperQuality from './SuperQuality'
+import CustomerReview from './CustomerReview'
+
+export {
+	Hero,
+	PopularProduct,
+	Footer,
+	Services,
+	SpecialOffer,
+	Subscribe,
+	SuperQuality,
+	CustomerReview,
+}
